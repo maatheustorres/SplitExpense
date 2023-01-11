05379bb35cddc3f5f3715cd3b5b8d07d4f8f1b54
Matheus Torres dos Reis <matheus.torres97@outlook.com>
2023-01-11 20:28:11 -0300

    Criação da classe de Result, ResultT e Error

---
+++ SplitExpense.Domain/Core/Primitives/Error.cs
@@ -0,0 +1,26 @@
+﻿namespace SplitExpense.Domain.Core.Primitives;
+
+public sealed class Error : ValueObject 
+{
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+    public string Message { get; }
+
+    public static implicit operator string(Error error) => error?.Code ?? string.Empty;
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Code;
+        yield return Message;
+    }
+
+    /// <summary>
+    /// Gets the empty error instance.
+    /// </summary>
+    internal static Error None => new Error(string.Empty, string.Empty);
+}
---
+++ SplitExpense.Domain/Core/Primitives/Result/ResultT.cs
@@ -0,0 +1,16 @@
+﻿namespace SplitExpense.Domain.Core.Primitives.Result;
+
+public class ResultT<TValue> : Result
+{
+    private readonly TValue _value;
+
+    protected internal ResultT(TValue value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+        => _value = value;
+
+    public static implicit operator ResultT<TValue>(TValue value) => Success(value);
+
+    public TValue Value => IsSuccess
+        ? _value
+        : throw new InvalidOperationException("The value of a failure result can not be accessed");
+}
