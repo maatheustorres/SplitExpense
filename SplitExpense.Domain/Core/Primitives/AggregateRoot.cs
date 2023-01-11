af0ea39af33829ab240749db12dfeff2328bd360
Matheus Torres dos Reis <matheus.torres97@outlook.com>
2023-01-11 16:05:02 -0300

    Criacao dos primitivos

---
+++ SplitExpense.Domain/Core/Primitives/AggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿namespace SplitExpense.Domain.Core.Primitives;
+
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot(Guid Id)
+        : base(Id)
+    {
+    }
+
+    protected AggregateRoot()
+    {
+    }
+}
